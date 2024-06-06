6f28b6ff98ab42c7ca8470103fbbdd110ed1817a
Rania <saadirania406@gmail.com>
2024-06-06 09:26:09 +0100

    fix bug

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -42,10 +42,6 @@ export default function Layout() {
 
             setifram(url)
             setvalue(url)
-        }else{
-            console.log('iframe none')
-            setifram('')
-            setvalue('')
         }
     }, [location.search]);
     return (
